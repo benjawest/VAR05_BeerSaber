625e0a610406c0613a7c819df447aad09516852b
Benjamin Westergreen <65360736+benjawest@users.noreply.github.com>
2023-06-29 21:12:44 -0700

    Created Master Scene | Updating metronome with play/pause/restart functions

--- Assets/BeerSaber/BenScripts/Metronome.cs
+++ Assets/BeerSaber/BenScripts/Metronome.cs
@@ -15,37 +15,52 @@ public class Metronome : MonoBehaviour
     private float elapsedTimeMinutes => (float)(elapsedAudioTime / 60); // This is the time since the audio source started playing, in minutes
     public int currentBeat => Mathf.FloorToInt(elapsedTimeMinutes * BPM); // This is the current beat, rounded down
 
+    public bool isPaused;
+    public bool songCompletePlay = false;
+
     private void Start()
     {
-        double currentAudioTime = AudioSettings.dspTime; // This is the current time according to the audio source
-        audioStartTime = (float)currentAudioTime + 3; // Applying an offset to the start time to account for the engine to start up first
-        songSource.PlayScheduled(audioStartTime); // Play the audio source at the scheduled start time
+        
 
     }
 
     private void Update()
     {
-        // Use the Unity time for now, even though it WOULD desync from a song!
-        // float time = Time.time;
-
-        double time = elapsedAudioTime;  // This is the time since the audio source started playing
-
-        // Convert it from seconds to minutes, e.g., 30 seconds = 0.5 minutes.
-        // float toMinutes = elapsedTimeMinutes;
-
-        // Convert from seconds to beat, rounding down (e.g., 45.67 seconds = beat 45 at 60 bpm).
-        int beat = currentBeat;
-
-        // If the beat has changed, update the color of the target.
-        if (previousBeat != beat)
+        if(!isPaused) // If the metronome is not paused, update the time and beat
         {
-            //target.GetComponent<Renderer>().material.color = beat % 2 != 0 ? Color.red : Color.blue;
+            if (!songSource.isPlaying) // If the song is done playing, stop the metronome and show the song complete screen
+            {
+                songCompletePlay = true;
+            }
+            
+            double time = elapsedAudioTime;  // This is the time since the audio source started playing
 
-            previousBeat = beat; // Update the previous beat to the current beat
+            // Convert from seconds to beat, rounding down (e.g., 45.67 seconds = beat 45 at 60 bpm).
+            int beat = currentBeat;
+
+            // If the beat has changed, update the color of the target.
+            if (previousBeat != beat)
+            {
+                previousBeat = beat; // Update the previous beat to the current beat
+            }
+
+            string output = $"Time: <b>{time:00:00.00}</b> Beat: <b>{beat:000}</b>";
+            timeText.text = output;
         }
+    }
 
-        string output = $"Time: <b>{time:00:00.00}</b> Beat: <b>{beat:000}</b>";
-        timeText.text = output;
+    public void StartMetronome()
+    {
+        double currentAudioTime = AudioSettings.dspTime; // This is the current time according to the audio source
+        audioStartTime = (float)currentAudioTime + 3; // Applying an offset to the start time to account for the engine to start up first
+        songSource.PlayScheduled(audioStartTime); // Play the audio source at the scheduled start time
+        isPaused = false;
+    }
+
+    public void RestartMetronome() // Not tested yet
+    {
+        songSource.Stop();
+        StartMetronome();
     }
 
 }
